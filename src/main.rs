10f5afff155511f8e6568cc4d68b3171a6f637b7
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-03-22 22:35:40 +0700

    Initial setup

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate leptos;
+extern crate thaw;
+
+use leptos::prelude::*;
+use thaw::*;
+
+
+fn main() {
+    mount_to_body(App);
+}
+
+
+
+#[component]
+pub fn App() -> impl IntoView{
+    view! {
+        
+    }
+}
